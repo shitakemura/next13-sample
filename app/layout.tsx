e91149a5a07a6cf79545d7e131857f7fa5412c2d
shitakemura <shitakemura@gmail.com>
2022-10-28 22:07:44 +0900

    Setup tailwind css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,21 @@
 import Link from 'next/link'
+import 'tailwindcss/tailwind.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
+    <html className="bg-gray-900 text-gray-100 antialiased">
       <head>
         <title>Nested layouts by example</title>
       </head>
 
       <body>
-        <header>
-          <nav>
+        <header className="border-b p-4">
+          <nav className="space-x-4">
             <Link href="/">Home</Link>
             <Link href="/movies">Movies</Link>
           </nav>
         </header>
-        {children}
+        <div className="p-4">{children}</div>
       </body>
     </html>
   )
