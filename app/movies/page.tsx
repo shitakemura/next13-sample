d7c1a22659b57be6110efaa78fa330e872158fcf
shitakemura <shitakemura@gmail.com>
2022-10-29 21:44:11 +0900

    Use use to fetch movies data

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,3 +1,20 @@
+import { use } from 'react'
+import { Movie } from '../../types/movie'
+
+async function getMovies() {
+  const res = await fetch('http://localhost:3000/api/movies')
+
+  return res.json()
+}
+
 export default function Page() {
-  return <p>Movies page</p>
+  const movies = use(getMovies())
+
+  return (
+    <ul>
+      {movies.map((movie: Movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+  )
 }
--- pages/api/movies.ts
+++ pages/api/movies.ts
@@ -1,26 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import type { Movie } from '../../types/movie'
 
-type Movie = {
-  id: string
-  title: string
-  year: number
-  description: string
-}
-
-type Data = {
-  movies: Movie[]
-}
+type Data = Movie[]
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({
-    movies: [
-      {
-        id: '1',
-        title: 'Load of the Rings',
-        year: 2001,
-        description: 'This is a good movie',
-      },
-      { id: '2', title: 'Star Wars', year: 1977, description: 'This is a great movie' },
-    ],
-  })
+  res.status(200).json([
+    {
+      id: '1',
+      title: 'Load of the Rings',
+      year: 2001,
+      description: 'This is a good movie',
+    },
+    { id: '2', title: 'Star Wars', year: 1977, description: 'This is a great movie' },
+  ])
 }
