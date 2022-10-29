8f5599430d7e8feaa3a3169019857c5912c52953
shitakemura <shitakemura@gmail.com>
2022-10-29 21:58:31 +0900

    Change movie page to layout

---
+++ app/movies/layout.tsx
@@ -0,0 +1,24 @@
+import React, { use } from 'react'
+import { Movie } from '../../types/movie'
+
+async function getMovies() {
+  const res = await fetch('http://localhost:3000/api/movies')
+
+  return res.json()
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const movies = use(getMovies())
+
+  return (
+    <div className="flex">
+      <ul className="pr-10 text-sm">
+        {movies.map((movie: Movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+
+      <div>{children}</div>
+    </div>
+  )
+}
