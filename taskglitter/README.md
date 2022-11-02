473bcba88fef2e722bd01d711ca7e25678659709
shitakemura <shitakemura@gmail.com>
2022-11-03 07:59:19 +0900

    Create project

---
+++ taskglitter/README.md
@@ -0,0 +1,3 @@
+## Next.js x Prisma x Supabase
+
+- Amy Dutton: How Next.js and Prisma make frontend developers full-stack
---
+++ taskglitter/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Projects | Task Glitter</title>
+      </Head>
+      <div>Hello, world</div>
+    </>
+  )
+}
