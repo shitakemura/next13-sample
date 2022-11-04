e3f735f33e1c4d47814ed5bb17ec074591c2b2f8
shitakemura <shitakemura@gmail.com>
2022-11-05 00:00:24 +0900

    Add fetching projects

--- taskglitter/src/pages/index.tsx
+++ taskglitter/src/pages/index.tsx
@@ -1,12 +1,31 @@
+import type { GetServerSideProps } from 'next'
+import type { Prisma } from '@prisma/client'
 import Head from 'next/head'
+import prisma from '@/lib/prisma'
 
-export default function Home() {
+type Props = {
+  projects: Prisma.ProjectsSelect[]
+}
+
+export default function Home({ projects }: Props) {
+  console.log(`projects: ${JSON.stringify(projects)}`)
+
   return (
     <>
-      <Head>
-        <title>Projects | Task Glitter</title>
-      </Head>
+      <title>Projects | Task Glitter</title>
+      <meta
+        name='description'
+        content='Adding a little sparkle to your everyday to dos.'
+      />
+      <link rel='icon' href='/favicon.ico' />
       <div>Hello, world</div>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const projects = await prisma.projects.findMany()
+  return {
+    props: { projects },
+  }
 }
